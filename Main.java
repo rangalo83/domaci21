baca168861baf1ac08c25a43a16129bfa3d2b688
rangalo83 <goranpincir@gmail.com>
2022-09-21 14:02:30 +0200

    Adding Java file

---
+++ Main.java
@@ -0,0 +1,21 @@
+package D_21_Goran_Pincir_Zadatak1;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+
+    /*
+    Kreirati public repository (projekat) na github-u, nazvati ga proizvoljno.
+    Kroz IntelliJ dodati 1 fajl, nebitnog tipa, moze txt, moze slika, meme, java...
+    Napraviti jedan commit sa porukom "my first commit", koji ce da sadrzi taj fajl (git add)
+    Uraditi git push.
+    Uraditi isto to jos 2 puta (tako da ima 3 commita ukupno), dodati ili izmeniti neki fajl, imenovati commit sa odgovarajucom porukom sta ste izmenili/dodali.
+    To je to za IntelliJ. Uraditi iste stavke sa komandnom linijom.
+
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+
+}
